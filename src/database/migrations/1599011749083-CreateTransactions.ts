01e6680bb8b29f52ca05981b82efa1dbdde3c1a5
Augusto Calixto <gutodenovaes@outlook.com>
2020-09-03 16:09:58 -0300

    Desafio concluido

---
+++ src/database/migrations/1599011749083-CreateTransactions.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransactions1599011749083
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'value',
+            type: 'float',
+            isNullable: false,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const allBalance = await this.find();
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+    allBalance.forEach((transaction: Transaction) => {
+      if (transaction.type === `income`) {
+        balance.income += transaction.value;
+        balance.total += transaction.value;
+      }
+      if (transaction.type === `outcome`) {
+        balance.outcome += transaction.value;
+        balance.total -= transaction.value;
+      }
+    });
+    return balance;
   }
 }
 
